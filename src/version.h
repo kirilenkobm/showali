25902779133629598bec477b9760b32f388f39eb
kirilenkobm <kirilenkobm@gmail.com>
2025-07-10 22:41:33 +0200

    MVP

---
+++ src/version.h
@@ -0,0 +1,18 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+
+#define VERSION_STRING "0.0.1"
+#define VERSION_BUILD __DATE__ " " __TIME__
+
+// Numeric version for comparisons
+#define VERSION_NUMBER ((VERSION_MAJOR << 16) | (VERSION_MINOR << 8) | VERSION_PATCH)
+
+// Program info
+#define PROGRAM_NAME "showali"
+#define PROGRAM_DESCRIPTION "TUI alignment viewer"
+
+#endif // VERSION_H 
